de8e0fc94c81aa35244852155ddd3c32e5e5d100
Maksim Kot <kotmaksimarturovich@gmail.com>
2023-09-14 16:59:59 +0300

    Implement Average thread, fix average issue in laba_2

--- os_laba_2/os_laba_2/os_laba_2.cpp
+++ os_laba_2/os_laba_2/os_laba_2.cpp
@@ -1,22 +1,22 @@
 #include <windows.h>
 #include <iostream>
-using namespace std;
+#include <iomanip>
 
 struct Array
 {
-	int* a;
+	double* a;
 	int size;
-	int average;
+	double average;
 	int min_;
 	int max_;
-	Array (int* a_, int size_): a(a_), size(size_), average(0), min_(0), max_(0){}
+	Array (double* a_, int size_): a(a_), size(size_), average(0), min_(0), max_(0){}
 };
 
 DWORD WINAPI Min_Max(LPVOID arr)
 {
-	cout << "Thread Min_Max is started." << endl;
-	int min_ = static_cast<Array*>(arr)->a[0];
-	int max_ = min_;
+	std::cout << "Thread Min_Max is started.\n";
+	double min_ = static_cast<Array*>(arr)->a[0];
+	double max_ = min_;
 	int index_min = 0, index_max = 0;
 	for (int i = 1; i < static_cast<Array*>(arr)->size; i++)
 	{
@@ -35,8 +35,25 @@ DWORD WINAPI Min_Max(LPVOID arr)
 	static_cast<Array*>(arr)->max_ = index_max;
 	static_cast<Array*>(arr)->min_ = index_min;
 
-	cout << "Min: " << min_ << '\n' << "Max: " << max_ << '\n';
-	cout << "Thread Min_Max is finished." << endl;
+	std::cout << "Min: " << min_ << '\n' << "Max: " << max_ << '\n' 
+		<< "Thread Min_Max is finished.\n";
+	return 0;
+}
+
+DWORD WINAPI Average(LPVOID arr)
+{
+	std::cout << "Thread Average is started.\n";
+	double sum = 0;
+	for (int i = 0; i < static_cast<Array*>(arr)->size; i++)
+	{
+		sum += static_cast<Array*>(arr)->a[i];
+		Sleep(12);
+	}
+	static_cast<Array*>(arr)->average = sum / static_cast<Array*>(arr)->size;
+	
+
+	std::cout << "Average: " << static_cast<Array*>(arr)->average << '\n' 
+		<< "Thread Average is finished.\n";
 	return 0;
 }
 
@@ -45,13 +62,13 @@ int main()
 	HANDLE hThread;
 	DWORD IDThread;
 	int n;
-	cout << "Enter count of elements: ";
-	cin >> n;
-	int* a = new int[n];
-	cout << "Enter elements:\n";
+	std::cout << "Enter count of elements: ";
+	std::cin >> n;
+	double* a = new double[n];
+	std::cout << "Enter elements:\n";
 	for (int i = 0; i < n; ++i)
 	{
-		cin >> a[i];
+		std::cin >> a[i];
 	}
 	auto param = new Array(a, n);
 	hThread = CreateThread(NULL, 0, Min_Max, (LPVOID)param, 0, &IDThread);
@@ -59,5 +76,24 @@ int main()
 		return GetLastError();
 	WaitForSingleObject(hThread, INFINITE);
 	CloseHandle(hThread);
+
+	HANDLE hAverage;
+	DWORD IDAverage;
+
+	hAverage = CreateThread(NULL, 0, Average, (LPVOID)param, 0, &IDAverage);
+	if (hAverage == NULL)
+		return GetLastError();
+	WaitForSingleObject(hAverage, INFINITE);
+	CloseHandle(hAverage);
+
+	double max_ = param->a[param->max_], min_ = param->a[param->min_];
+	std::cout << "New array: ";
+	for (int i = 0; i < param->size; i++)
+	{
+		if (param->a[i] == max_ || param->a[i] == min_)
+			param->a[i] = param->average;
+		std::cout << std::setprecision(3) << param->a[i] << " ";
+	}
+	std::cout << '\n';
 	return 0;
 }
