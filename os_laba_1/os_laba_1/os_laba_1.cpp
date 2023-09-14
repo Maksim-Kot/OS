e21724fa0751d52f1c68eff47813858f039efaea
Maksim Kot <kotmaksimarturovich@gmail.com>
2023-09-14 13:17:18 +0300

    Create "Creator"  and "main".cpp

---
+++ os_laba_1/os_laba_1/os_laba_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void Console_report(char* name) 
+{
+	
+}
+
+char* InputForCreator(std::istream& is, char* binfile_name, int& number) 
+{
+	
+}
+
+char* InputForReporter(std::istream& is, char* binfile_name, char* reportfile_name, double& payment_by_hour) 
+{
+	
+}
+
+int main()
+{
+	return 0;
+}
