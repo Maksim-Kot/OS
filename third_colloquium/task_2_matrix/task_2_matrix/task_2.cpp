90e613adfad5584bf69f4373183c5f3ce19d94a4
Maksim Kot <kotmaksimarturovich@gmail.com>
2023-11-26 01:34:11 +0300

    Complete task_2 and added tests

--- third_colloquium/task_2_matrix/task_2_matrix/task_2.cpp
+++ third_colloquium/task_2_matrix/task_2_matrix/task_2.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <windows.h>
 #include <iomanip>
-using namespace std;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::ios_base;
+using std::cin;
+using std::cout;
 CRITICAL_SECTION criticalSection;
 HANDLE* hEvent;
 HANDLE hEventT;
 int temp;
-
 
 struct MMatrix
 {
@@ -16,8 +22,17 @@ struct MMatrix
     vector<vector<double>> res;
     int i, j;
     int rows1, cols1, rows2, cols2;
+    int amount;
+    string nameOfFile;
 
-    MMatrix(vector<vector<double>> a, vector<vector<double>> b)
+    MMatrix()
+    {
+        rows1 = 0;
+        cols1 = 0;
+        rows2 = 0;
+        cols2 = 0;
+    }
+    MMatrix(vector<vector<double>> a, vector<vector<double>> b, int count, string s)
     {
         mat1 = a; mat2 = b;
         i = j = 0;
@@ -26,17 +41,18 @@ struct MMatrix
         rows2 = mat2.size();
         cols2 = mat2[0].size();
         res.resize(rows1);
-        for(int i = 0; i < rows1; ++i)
+        amount = count;
+        nameOfFile = s;
+        for (int i = 0; i < rows1; ++i)
         {
             res[i].resize(cols2, 0);
         }
     }
 };
 
-
 DWORD WINAPI FindElement(LPVOID mmatrix)
 {
-    if (temp < 2)
+    if (temp < (reinterpret_cast<MMatrix*>(mmatrix))->amount)
     {
         SetEvent(hEvent[temp]);
         ++temp;
@@ -46,18 +62,18 @@ DWORD WINAPI FindElement(LPVOID mmatrix)
     while (b)
     {
         std::cout << "Thread FindElment is started.\n";
-        EnterCriticalSection(&criticalSection);
-        int i = ((MMatrix*)mmatrix)->i;
-        int j = ((MMatrix*)mmatrix)->j;
-        if (i < ((MMatrix*)mmatrix)->rows1)
+        EnterCriticalSection(&criticalSection); //Suspend all threads to retrieve indexes
+        int i = (reinterpret_cast<MMatrix*>(mmatrix))->i;
+        int j = (reinterpret_cast<MMatrix*>(mmatrix))->j;
+        if (i < (reinterpret_cast<MMatrix*>(mmatrix))->rows1)
         {
-            ((MMatrix*)mmatrix)->j = (j + 1) % ((MMatrix*)mmatrix)->cols2;
-            if (0 == ((MMatrix*)mmatrix)->j) ++((MMatrix*)mmatrix)->i;
-            LeaveCriticalSection(&criticalSection);
+            (reinterpret_cast<MMatrix*>(mmatrix))->j = (j + 1) % (reinterpret_cast<MMatrix*>(mmatrix))->cols2;
+            if (0 == (reinterpret_cast<MMatrix*>(mmatrix))->j) ++(reinterpret_cast<MMatrix*>(mmatrix))->i;
+            LeaveCriticalSection(&criticalSection); //then open access
 
-            for (int k = 0; k < ((MMatrix*)mmatrix)->cols1; ++k)
+            for (int k = 0; k < (reinterpret_cast<MMatrix*>(mmatrix))->cols1; ++k)
             {
-                ((MMatrix*)mmatrix)->res[i][j] += ((MMatrix*)mmatrix)->mat1[i][k] * ((MMatrix*)mmatrix)->mat2[k][j];
+                (reinterpret_cast<MMatrix*>(mmatrix))->res[i][j] += (reinterpret_cast<MMatrix*>(mmatrix))->mat1[i][k] * (reinterpret_cast<MMatrix*>(mmatrix))->mat2[k][j];
             }
         }
         else
@@ -71,13 +87,46 @@ DWORD WINAPI FindElement(LPVOID mmatrix)
     return 0;
 }
 
-
-// Функция для вывода матрицы
-void printMatrix(const vector<vector<double>>& mat)
+DWORD WINAPI PrintMatrix(LPVOID mmatrix)
 {
-    for (const auto& row : mat) 
+    ofstream fout((reinterpret_cast<MMatrix*>(mmatrix))->nameOfFile, ios_base::app);
+    if (!fout.is_open())    //check if the structure is empty
     {
-        for (double val : row) 
+        throw "Cannot open file";
+    }
+    fout << "\n";
+    if ((reinterpret_cast<MMatrix*>(mmatrix))->res.empty())
+    {
+        throw "Empty matrix";
+    }
+    for (int i = 0; i < (reinterpret_cast<MMatrix*>(mmatrix))->rows1; ++i)
+    {
+        if ((reinterpret_cast<MMatrix*>(mmatrix))->res[i].empty())
+        {
+            throw "Empty matrix";
+        }
+        for (int j = 0; j < (reinterpret_cast<MMatrix*>(mmatrix))->cols2; ++j)
+        {
+            fout << (reinterpret_cast<MMatrix*>(mmatrix))->res[i][j] << " ";
+        }
+        fout << "\n";
+    }
+    return 0;
+}
+
+void PrintMatrixOnConsole(const vector<vector<double>>& mat)
+{
+    if (mat.empty())    //check if the structure is empty
+    {
+        throw "Empty matrix";
+    }
+    for (const auto& row : mat)
+    {
+        if (row.empty())
+        {
+            throw "Empty matrix";
+        }
+        for (double val : row)
         {
             cout << val << " ";
         }
@@ -85,12 +134,45 @@ void printMatrix(const vector<vector<double>>& mat)
     }
 }
 
-int main() 
+vector<vector<double>> GetMatrix(ifstream& fin)
+{
+    int a, b;
+    fin >> a >> b;
+    if (a <= 0 || b <= 0)   //check if the input is empty
+    {
+        throw "Empty matrix";
+    }
+    vector<vector<double>> matrix(a, vector<double>(b));
+    for (int i = 0; i < a; ++i)
+    {
+        for (int j = 0; j < b; ++j)
+        {
+            fin >> matrix[i][j];
+        }
+    }
+    return matrix;
+}
+
+void GoodToMultiply(MMatrix& a)
+{
+    if (a.cols1 != a.rows2)
+    {
+        throw "Matrix cannot be multiplied.";   //count of colomn first matrix not equals 
+    }                                           //count of rows second matrix
+}
+
+int main(int argc, char* argv[])
 {
     InitializeCriticalSection(&criticalSection);
+    cout << "Enter name of file:\n";
+    string s;
+    s = argv[1];
+    //cin >> s;
+    ifstream fin(s);
     cout << "Enter amount of thread:\n";
     int amount;
-    cin >> amount;
+    amount = atoi(argv[2]);
+    //cin >> amount;
     hEvent = new HANDLE[amount];
     for (int i = 0; i < amount; ++i)
     {
@@ -98,23 +180,15 @@ int main()
     }
     hEventT = CreateEvent(NULL, TRUE, 0, NULL);
     temp = 0;
-    setlocale(LC_ALL, "rus");
-    vector<vector<double>> matrix1 = { {1, 2, 3},
-                                   {4, 5, 6},
-                                   {7, 8, 9} };
-    vector<vector<double>> matrix2 = { {9, 8, 7},
-                                   {6, 5, 4},
-                                   {3, 2, 1} };
 
+    vector<vector<double>> matrix1 = GetMatrix(fin);
+    vector<vector<double>> matrix2 = GetMatrix(fin);
 
-    MMatrix matrix(matrix1, matrix2);
+    fin.close();
 
-    if (matrix.cols1 != matrix.rows2)
-    {
-        cout << "Невозможно умножить матрицы: количество столбцов первой матрицы не равно количеству строк второй матрицы.\n";
-        return 0;
-    }
-    
+    MMatrix matrix(matrix1, matrix2, amount, s);
+
+    GoodToMultiply(matrix);
 
     HANDLE* hThread;
     DWORD* IDThread;
@@ -129,25 +203,34 @@ int main()
             return GetLastError();
     }
 
-    WaitForMultipleObjects(2, hEvent, TRUE, INFINITE);
-    SetEvent(hEventT);
-    WaitForMultipleObjects(2, hThread, TRUE, INFINITE);
+    WaitForMultipleObjects(amount, hEvent, TRUE, INFINITE); //Waiting for opening all threads
+    SetEvent(hEventT);                                      //Signal the start of work 
+    WaitForMultipleObjects(amount, hThread, TRUE, INFINITE);//Waiting for closing all threads
 
-    cout << "Первая матрица:\n";
-    printMatrix(matrix1);
-    cout << "\nВторая матрица:\n";
-    printMatrix(matrix2);
+    cout << "First matrix:\n";
+    PrintMatrixOnConsole(matrix1);
+    cout << "\nSecond matrix:\n";
+    PrintMatrixOnConsole(matrix2);
 
     vector<vector<double>> result = matrix.res;
 
-    cout << "\nРезультат умножения:\n";
-    printMatrix(result);
+    cout << "\nMultiplication result:\n";
+    PrintMatrixOnConsole(result);
+    HANDLE OutThread = CreateThread(NULL, 0, PrintMatrix, (LPVOID)&matrix, 0, &IDThread[0]);
+    if (OutThread == NULL)
+        return GetLastError();
 
+    WaitForSingleObject(OutThread, INFINITE);
 
     for (int i = 0; i < amount; ++i)
     {
         CloseHandle(hThread[i]);
+        CloseHandle(hEvent[i]);
     }
+    delete[] hThread;
+    delete[] hEvent;
+    CloseHandle(OutThread);
+    CloseHandle(hEventT);
     DeleteCriticalSection(&criticalSection);
     return 0;
 }
